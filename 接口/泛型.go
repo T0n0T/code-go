6a475d43d1087d042769df41c48fea6e9549aa8c
T0n0T <823478402@qq.com>
2023-01-31 17:57:29 +0800

    泛型

--- 接口/泛型.go
+++ 接口/泛型.go
@@ -4,11 +4,29 @@ import "fmt"
 
 func main() {
 	str := []string{"ssss", "hello"}
-	printfstring(str)
+	PrintfInterface(str)
 }
 
 // T泛型,预定义类型都可以传入
-func printfstring[T string | int](a []T) {
+func PrintfStringOrInt[T string | int](a []T) {
+	for _, a := range a {
+		fmt.Println(a)
+	}
+}
+
+func PrintfAny[T any](a []T) {
+	for _, a := range a {
+		fmt.Println(a)
+	}
+}
+
+// func PrintfInterface(a []interface{}) {
+// 	for _, a := range a {
+// 		fmt.Println(a)
+// 	}
+// }
+
+func PrintfInterface[T interface{}](a []T) {
 	for _, a := range a {
 		fmt.Println(a)
 	}
