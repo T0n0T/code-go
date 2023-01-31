66fdb6c1c1c70c9d98118f6aa87442afb8071a15
T0n0T <823478402@qq.com>
2023-01-31 17:48:51 +0800

    泛型

--- 接口/泛型.go
+++ 接口/泛型.go
@@ -2,18 +2,14 @@ package main
 
 import "fmt"
 
-// slice,map,channel都是引用类型
-// 说明切片作为引用类型,传递的是内存空间中的值,同c语言的数组名称的含义
 func main() {
-	c := oper(1, 2, add)
-	fmt.Println(c)
+	str := []string{"ssss", "hello"}
+	printfstring(str)
 }
 
-func oper(a, b int, fun func(int, int) int) int {
-	result := fun(a, b)
-	return result
-}
-
-func add(a, b int) int {
-	return a + b
+// T泛型,预定义类型都可以传入
+func printfstring[T string | int](a []T) {
+	for _, a := range a {
+		fmt.Println(a)
+	}
 }
