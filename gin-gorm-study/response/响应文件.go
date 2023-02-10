e8fb3b930099bf1ab4298a2daa357f1112c17deb
T0n0T <823478402@qq.com>
2023-02-10 18:07:45 +0800

    文件上传

--- gin-gorm-study/response/响应文件.go
+++ gin-gorm-study/response/响应文件.go
@@ -2,10 +2,23 @@ package response
 
 import (
 	"fmt"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 func ResponseFile(ctx *gin.Context) {
-	fmt.Println("111")
+	file, err := ctx.FormFile("pre-upload")
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{
+			"message": err.Error(),
+		})
+		return
+	}
+	fmt.Println(file.Filename)
+	dst := fmt.Sprintf("./%s", file.Filename)
+	ctx.SaveUploadedFile(file, dst)
+	ctx.JSON(http.StatusOK, gin.H{
+		"message": fmt.Sprintf("'%s' upload success!", file.Filename),
+	})
 }
