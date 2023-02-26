09fa3b348f4c978d216c2e372b828bd1998b005a
T0n0T <823478402@qq.com>
2023-02-27 00:13:25 +0800

    TODO:原生go的input_event

--- device/device.go
+++ device/device.go
@@ -1,25 +1,33 @@
-package device
+package main
 
 import (
 	"fmt"
-	"io/ioutil"
-	"net/http"
 	"os"
+	"syscall"
 )
 
 func main() {
-	for _, url := range os.Args[1:] {
-		resp, err := http.Get(url)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
-			os.Exit(1)
-		}
-		b, err := ioutil.ReadAll(resp.Body)
-		resp.Body.Close()
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
-			os.Exit(1)
-		}
-		fmt.Printf("%s", b)
-	}
+	syscall.SYS_EVENTFD
+	os.OpenFile("/dev/input/event0", os.O_RDWR, 0)
+	fd, _ := syscall.Open("/dev/input/event0", syscall.O_RDWR, 0)
+	syscall.Write()
+	pid, _, _ := syscall.Syscall(39, 0, 0, 0) // 用不到的就补上 0
+	fmt.Println("Process id: ", pid)
 }
+
+// func main() {
+// 	for _, url := range os.Args[1:] {
+// 		resp, err := http.Get(url)
+// 		if err != nil {
+// 			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+// 			os.Exit(1)
+// 		}
+// 		b, err := ioutil.ReadAll(resp.Body)
+// 		resp.Body.Close()
+// 		if err != nil {
+// 			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+// 			os.Exit(1)
+// 		}
+// 		fmt.Printf("%s", b)
+// 	}
+// }
