d66b76f53c473b18ce2440d5f4235ca521a860c4
T0n0T <823478402@qq.com>
2023-05-06 18:01:41 +0800

    fix: :zap: ping实例修改

--- high/net/ping/ping.go
+++ high/net/ping/ping.go
@@ -2,23 +2,81 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"os/signal"
+	"time"
 
 	"github.com/go-ping/ping"
+	probing "github.com/prometheus-community/pro-bing"
 )
 
+type PingWriter interface {
+	Write(msg string)
+}
+
+type testping struct{}
+
+func (*testping) Write(msg string) {
+	fmt.Println(msg)
+}
+
 func main() {
-	pinger, err := ping.NewPinger("www.google.com")
+	// c := make(chan os.Signal, 1)
+	// signal.Notify(c, os.Interrupt)
+	c := make(chan struct{}, 3)
+	Ping("192.168.1.1", c, &testping{})
+	// Ping2("192.168.1.1", c)
+}
+
+func Ping(host string, c chan struct{}, Writer PingWriter) (err error) {
+	pinger, err := probing.NewPinger(host)
 	if err != nil {
-		panic(err)
+		return
 	}
 
-	// Listen for Ctrl-C.
-	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt)
+	// listen
 	go func() {
-		for _ = range c {
+		for range c {
+			pinger.Stop()
+		}
+	}()
+
+	pinger.OnRecv = func(pkt *probing.Packet) {
+		Writer.Write(fmt.Sprintf("%d bytes from %s: icmp_seq=%d time=%v ttl=%v\n",
+			pkt.Nbytes, pkt.IPAddr, pkt.Seq, pkt.Rtt, pkt.TTL))
+	}
+	pinger.OnDuplicateRecv = func(pkt *probing.Packet) {
+		Writer.Write(fmt.Sprintf("%d bytes from %s: icmp_seq=%d time=%v ttl=%v (DUP!)\n",
+			pkt.Nbytes, pkt.IPAddr, pkt.Seq, pkt.Rtt, pkt.TTL))
+	}
+	pinger.OnFinish = func(stats *probing.Statistics) {
+		Writer.Write(fmt.Sprintf("\n--- %s ping statistics ---\n", stats.Addr))
+		Writer.Write(fmt.Sprintf("%d packets transmitted, %d packets received, %d duplicates, %v%% packet loss\n",
+			stats.PacketsSent, stats.PacketsRecv, stats.PacketsRecvDuplicates, stats.PacketLoss))
+		Writer.Write(fmt.Sprintf("round-trip min/avg/max/stddev = %v/%v/%v/%v\n",
+			stats.MinRtt, stats.AvgRtt, stats.MaxRtt, stats.StdDevRtt))
+	}
+
+	pinger.Count = -1
+	pinger.Size = 24
+	pinger.Interval = time.Second
+	pinger.Timeout = time.Second * 10
+	pinger.TTL = 64
+	pinger.SetPrivileged(false)
+
+	Writer.Write(fmt.Sprintf("PING %s (%s):\n", pinger.Addr(), pinger.IPAddr()))
+
+	err = pinger.Run()
+	return
+}
+
+func Ping2(host string, c chan struct{}) (err error) {
+	pinger, err := ping.NewPinger(host)
+	if err != nil {
+		return
+	}
+
+	// listen
+	go func() {
+		for range c {
 			pinger.Stop()
 		}
 	}()
@@ -42,8 +100,8 @@ func main() {
 	}
 
 	fmt.Printf("PING %s (%s):\n", pinger.Addr(), pinger.IPAddr())
-	err = pinger.Run()
-	if err != nil {
-		panic(err)
-	}
+
+	pinger.SetPrivileged(false)
+
+	return pinger.Run()
 }
--- utils/main/main.go
+++ utils/main/main.go
@@ -6,8 +6,9 @@ import (
 )
 
 func main() {
-	// err := utils.ZipFiles("./utils/aaa/123.zip", []string{"./utils/aaa/1.txt", "./utils/aaa/2.txt", "./utils/aaa/3.txt"}, "./utils/aaa/", ".")
-	a, err := utils.Unzip("./utils/aaa/123.zip", "./utils/aaa/")
-	fmt.Println(a)
+	// err := utils.ZipFiles("./aaa/123.zip", []string{"./aaa/1.txt", "./aaa/2.txt", "./aaa/3.txt"}, "./aaa/", "")
+	// _, err := utils.Unzip("./aaa/123.zip", "./aaa/")
+
+	err := utils.ZipFiles("backup.zip", []string{"/postgresql/backup/db.dump"}, "/postgresql/backup/", "")
 	fmt.Println(err)
 }
